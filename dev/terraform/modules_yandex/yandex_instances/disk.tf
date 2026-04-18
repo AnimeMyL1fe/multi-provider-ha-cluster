3c180b4c1decf223a02fd6f157ec9280d279fade
Alexander <sashan.mail.ru98@gmail.com>
2026-04-18 20:18:51 +0500

    fix folder_id

--- dev/terraform/modules_yandex/yandex_instances/disk.tf
+++ dev/terraform/modules_yandex/yandex_instances/disk.tf
@@ -1,10 +1,10 @@
 resource "yandex_compute_disk" "vm_disk" {
-  for_each = var.vm_configuration
-  name     = each.value.disk_name
-  type     = each.value.disk_type
-  zone     = var.yandex_zone
-  image_id = data.yandex_compute_image.ubuntu_image.id
-  size     = var.vms_profile[each.value.vm_profile].disk_size
+  for_each  = var.vm_configuration
+  name      = each.value.disk_name
+  type      = each.value.disk_type
+  zone      = var.yandex_zone
+  image_id  = data.yandex_compute_image.ubuntu_image.id
+  size      = var.vms_profile[each.value.vm_profile].disk_size
 
   labels = {
     environment = "ubuntu-disk"
